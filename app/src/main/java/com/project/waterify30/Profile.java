27d4fa40b27e53fe6ac02728d196c768a4e2ebaf
patricia <patriciaov2004@gmail.com>
2023-05-10 12:51:49 +0200

    commit profile 1

--- app/src/main/java/com/project/waterify30/Profile.java
+++ app/src/main/java/com/project/waterify30/Profile.java
@@ -3,12 +3,25 @@ package com.project.waterify30;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Profile extends AppCompatActivity {
 
+    private ImageButton buttonClose;
+    private Logic logic = new Logic();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        buttonClose = (ImageButton)findViewById(R.id.button6);
+        buttonClose.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                logic.openActivity(Profile.this, Homepage.class);
+            }
+        });
     }
 }
