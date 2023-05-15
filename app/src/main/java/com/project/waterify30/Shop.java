06966e55ed66f9c84f7ca3bf9b885fc909940056
patricia <patriciaov2004@gmail.com>
2023-05-15 03:07:15 +0200

    shop + beginning sliding row

--- app/src/main/java/com/project/waterify30/Shop.java
+++ app/src/main/java/com/project/waterify30/Shop.java
@@ -3,12 +3,25 @@ package com.project.waterify30;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Shop extends AppCompatActivity {
 
+    private ImageButton buttonHome;
+    private Logic logic = new Logic();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+
+        buttonHome = (ImageButton) findViewById(R.id.home_button);
+        buttonHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                logic.openActivity(Shop.this, Homepage.class);
+            }
+        });
     }
 }
