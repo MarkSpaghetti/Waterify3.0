df0ba6a995a1653d3e5b481ff0ab927993455519
Marcelo <mribero69@gmail.com>
2023-05-22 15:21:52 +0200

    Marcelo working on Profile picture selecter

--- app/src/main/java/com/project/waterify30/Profile.java
+++ app/src/main/java/com/project/waterify30/Profile.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -13,6 +14,7 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class Profile extends AppCompatActivity {
 
+    ImageView profilePicture;
     private ImageButton buttonClose;
     FirebaseAuth auth;
     Button buttonSignout;
