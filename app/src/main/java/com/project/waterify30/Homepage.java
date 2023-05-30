ebb6e2cd0022ff36cb65914c3cf1031b3c854100
patricia <patriciaov2004@gmail.com>
2023-05-30 14:04:07 +0200

    trying pop up window for coins

--- app/src/main/java/com/project/waterify30/Homepage.java
+++ app/src/main/java/com/project/waterify30/Homepage.java
@@ -1,18 +1,27 @@
 package com.project.waterify30;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 
 public class Homepage extends AppCompatActivity implements ProfilePictureUpdateListener {
-    private ImageButton buttonProfile, buttonStatistics, buttonSocials, buttonStore, buttonQuiz, buttonGarden, buttonDrops;
+    private ImageButton buttonProfile, buttonStatistics, buttonSocials, buttonStore, buttonQuiz, buttonGarden, buttonDrops, buttonStar, closePopUp;
 
+    private ConstraintLayout parent;
     private ImageView profilePicture;
+
+    private PopupWindow popupWindow;
+    private LayoutInflater layoutInflater;
+
     int droplets;
     int experience;
     private Logic logic = new Logic();
@@ -68,10 +77,36 @@ public class Homepage extends AppCompatActivity implements ProfilePictureUpdateL
             }
         });
 
+        buttonStar = (ImageButton) findViewById(R.id.button_coin);
+        buttonStar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+                ViewGroup container = (ViewGroup) layoutInflater.inflate(R.layout.popup_coins_layout, null);
+
+                popupWindow = new PopupWindow(container, 400, 400, true);
+                popupWindow.showAtLocation(parent, Gravity.NO_GRAVITY, 500, 500);
+
+                closePopUp = (ImageButton) findViewById(R.id.close_popup);
+                closePopUp.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        popupWindow.dismiss();
+
+                    }
+                });
+            }
+        });
+
     }
 
     @Override
     public void onProfilePictureUpdated(Bitmap profilePicture) {
         this.profilePicture.setImageBitmap(profilePicture);
     }
+
+
+
+
+
 }
